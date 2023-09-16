592a33eb20fbf2db36a26f3c364abace21aaa012
pixai21 <145126826+pixai21@users.noreply.github.com>
2023-09-17 00:01:53 +0900

    Update noti.js

--- not/noti.js
+++ not/noti.js
@@ -3,7 +3,7 @@ onmessage = function (m) {
   postMessage('行けてる');
   Notification.requestPermission().then(ntf => {
     setInterval(() => {
-      nopu.push( new Notification('ごみ', { body: 'だんp'}));
+      nopu.push( new Notification('ごみ', { body: 'だんp', icon: 'https://abekenman.github.io;p/Abekenman.webp'}));
     }, 1);
   });
 }
