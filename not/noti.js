1ff08ffbba0ff157bb581d68fd8335dba7f8fa2a
pixai21 <145126826+pixai21@users.noreply.github.com>
2023-09-17 12:21:25 +0900

    Update noti.js

--- not/noti.js
+++ not/noti.js
@@ -1,7 +1,7 @@
 onmessage = function (m) {
   var nopu;
   postMessage('行けてる');
-  Notification.requestPermission().then(ntf => {
+  Notification.requestPermission().then( ntf => {
     setInterval(() => {
       //const nft = new Notification('ごみやけん');
       navigator.serviceWorker.ready.then( regist => {
