f99c36cb41170d8d837f011d6c19a1f2d4393764
pixai21 <145126826+pixai21@users.noreply.github.com>
2023-09-17 12:43:33 +0900

    Update not.js

--- not/not.js
+++ not/not.js
@@ -3,12 +3,11 @@ onload = () => {
   document.onclick = () => {
     try {
       ww = new Worker('noti.js');
-        alert(ww);
-        ww.onmessage = wm => {
-          alert(wm.data);
-        }
-        ww.postMessage('ごみまじ');
-      });
+      alert(ww);
+      ww.onmessage = wm => {
+        alert(wm.data);
+      }
+      ww.postMessage('ごみまじ');
     } catch (er) {
       alert(er);
     }
