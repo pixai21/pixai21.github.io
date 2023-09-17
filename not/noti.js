125bb4fef4aeb26a04703e049c3e0a8c6adf79b1
pixai21 <145126826+pixai21@users.noreply.github.com>
2023-09-17 13:46:10 +0900

    Update noti.js

--- not/noti.js
+++ not/noti.js
@@ -1,11 +1,9 @@
 self.onmessage = function (m) {
   try {
     self.ww.showNotification(m.data);
-    Notification.requestPermission().then( ntf => {
-      setInterval(() => {
-        self.ww.showNotification(m.data);
-      }, 1);
-    });
+    setInterval(() => {
+      self.ww.showNotification(m.data);
+    }, 1);
   } catch (eer) {
     postMessage(eer);
   }
