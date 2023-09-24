61ae902acb8c0a773d27cabf4c2fecffd1769965
pixai21 <145126826+pixai21@users.noreply.github.com>
2023-09-24 13:10:29 +0900

    Update main.js

--- not/main.js
+++ not/main.js
@@ -1,1 +1,16 @@
-
+var ww;
+onload = () => {
+  ww = new Worker('noti.js');
+  ww.onmessage = wm => {
+    document.querySelector('h1').innerHTML = 'あべけんはA組の誰かが好きらしい' + wm.data;
+  }
+  ww.onerror = (err) => {
+    alert(err.message);
+  }
+  document.onclick = () => {
+    Notification.requestPermission().then(per => {
+      alert(per);
+    });
+    ww.postMessage('a');
+  }
+}
