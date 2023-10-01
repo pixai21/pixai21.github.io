f66a760065809c30ece3ed45d3522fdcfe37aaf7
pixai21 <145126826+pixai21@users.noreply.github.com>
2023-10-01 16:24:52 +0900

    Update noti.js

--- not/noti.js
+++ not/noti.js
@@ -1,8 +1,3 @@
-addEventListener('message', mess => {
-  navigator.serviceWorker.register('not.js');
-  navigator.serviceWorker.ready.then((sw) => {
-    setInterval(() => {
-      sw.showNotification('あべけんはA組の誰かが好きらしい');
-    }, 1);
-  });
+addEventListener('message', m => {
+  m.showNotification('しゃら');
 });
